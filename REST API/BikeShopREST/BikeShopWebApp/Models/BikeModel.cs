6f8b033d2cf69f6f63b396e0098cd06bdd613538
H A L T <72382258+Kamil097@users.noreply.github.com>
2023-08-27 20:57:30 +0200

    Initialize web app

---
+++ REST API/BikeShopREST/BikeShopWebApp/Models/BikeModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
+
+namespace BikeShopWebApp.Models
+{
+    public class BikeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public float Price { get; set; }
+        public float Rating { get; set; }
+    }
+}
